c541fc7d1c8e59aaf86f2043ace096fc84f3706a
James Collins <james.collins@outlook.com.au>
2022-12-09 15:53:12 +1000

    deploy commands on startup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
+const { REST, Routes, Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
 const { token, webhookAddress, webhookPort, webhookSecret } = require('./config.json');
 const http = require('http');
 const qs = require('querystring');
@@ -13,6 +13,7 @@ const client = new Client({
 
 // Load commands
 client.commands = new Collection();
+const commands = []; 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
@@ -20,7 +21,28 @@ for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
 }
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+// Send command list
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
 
 // Load events
 const eventsPath = path.join(__dirname, 'events');
