aa2a0b4a086e8b6afba36658aa2cb8e19ded0cc3
James Collins <james.collins@outlook.com.au>
2022-12-16 15:33:51 +1000

    updated event to store author message counts

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,16 +1,24 @@
+const objectutils = require('js-object-utilities'); 
 const { gameChatChannelId } = require('../config.json');
 const api = require('../utils/api.js');
+const store = require('../utils/store.js');
 
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        if (gameChatChannelId && message.channelId == gameChatChannelId && !message.author.bot) {
-            api('chat', 'POST', {
-                'playername': message.author.username,
-                'message': message.content
-            }).then((response) => {
-                // console.log(`here, response: ${response.status}, data: ${JSON.stringify(response.json)}`);
-            });
+        if (!message.author.bot) {
+            if (gameChatChannelId && message.channelId == gameChatChannelId) {
+                api('chat', 'POST', {
+                    'playername': message.author.username,
+                    'message': message.content
+                }).then((response) => {
+                    // console.log(`here, response: ${response.status}, data: ${JSON.stringify(response.json)}`);
+                });
+            }
+
+            var data = store.load();
+            objects.increment(data, message.author.id + '.messages');
+            store.save(data);
         }
     },
 };
