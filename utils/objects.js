131dae3a65717d674ce3c40427defe0fb5b8ece7
James Collins <james.collins@outlook.com.au>
2022-12-16 21:40:22 +1000

    added delete function

--- utils/objects.js
+++ utils/objects.js
@@ -48,5 +48,31 @@ module.exports = {
         var val = module.exports.get(object, key, 0);
         val--;
         module.exports.set(object, key, val);
+    },
+
+    delete: function(object, keys) {
+        (Array.isArray(keys) ? keys : [keys]).forEach((key) => {
+            const keyParts = (typeof key === "number" ? `${key}` : key).split(".");
+            if (keyParts.length === 1) {
+                if (Array.isArray(object)) {
+                    object.splice(parseInt(keyParts[0]), 1);
+                }
+                else {
+                    delete object[keyParts[0]];
+                }
+            }
+            else {
+                const lastKey = keyParts.pop();
+                const nextLastKey = keyParts.pop();
+                const nextLastObj = keyParts.reduce((a, key) => a[key], object);
+                if (Array.isArray(nextLastObj[nextLastKey])) {
+                    nextLastObj[nextLastKey].splice(parseInt(lastKey), 1);
+                }
+                else if (typeof nextLastObj[nextLastKey] !== "undefined" && nextLastObj[nextLastKey] !== null) {
+                    delete nextLastObj[nextLastKey][lastKey];
+                }
+            }
+        });
+        return object;
     }
 }
