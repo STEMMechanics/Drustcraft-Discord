b2b9cdd10b6060dc4878f0926f421708247aa1c3
James Collins <james.collins@outlook.com.au>
2022-12-16 15:32:38 +1000

    updated api call

--- commands/who.js
+++ commands/who.js
@@ -1,10 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
+const api = require('../utils/api.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('who')
-		.setDescription('Who are you?'),
+		.setDescription('Who are you?')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('The user to lookup')
+				.setRequired(true)),
 	async execute(interaction) {
-		return interaction.reply('Not really sure who you are. Tried looking in the mirror?');
+		const discordUser = interaction.options.getUser('user');
+
+		api(`user?discord_id=${discordUser.id}`, 'POST', {}).then((response) => {
+			if (response.status == 200 && (response.json.hasOwnProperty('players'))) {
+				return interaction.reply(`${discordUser.username} is known as ${response.json.players[0].name}`);
+			} else {
+				const responses = [
+					'No idea bud',
+					'Never seen them playing',
+					'Stranger round those parts',
+					'Hides themselves well in game'
+				];
+
+				return interaction.reply(responses[Math.floor(Math.random() * responses.length)]);
+			}
+		});
 	},
 };
